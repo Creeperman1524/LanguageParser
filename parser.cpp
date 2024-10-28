292082677d8a533cc7d989fd0c7522c0587e139d
Anthony M <38088724+Creeperman1524@users.noreply.github.com>
2024-10-27 23:38:21 -0400

    Initial project setup
    Downloaded all necessary files and setup the project to be completed

---
+++ parser.cpp
@@ -0,0 +1,46 @@
+// =======================================================
+// Name        : parser.cpp
+// Description : Parses an input of tokens
+// =======================================================
+
+#include "parser.h"
+
+/*bool Prog(istream &in, int &line) {}*/
+
+/*bool StmtList(istream &in, int &line) {}*/
+
+/*bool Stmt(istream &in, int &line) {}*/
+
+/*bool DeclStmt(istream &in, int &line) {}*/
+
+/*bool VarList(istream &in, int &line) {}*/
+
+/*bool ControlStmt(istream &in, int &line) {}*/
+
+/*bool PrintStmt(istream &in, int &line) {}*/
+
+/*bool CompStmt(istream &in, int &line) {}*/
+
+/*bool IfStmt(istream &in, int &line) {}*/
+
+/*bool AssignStmt(istream &in, int &line) {}*/
+
+/*bool Var(istream &in, int &line) {}*/
+
+/*bool ExprList(istream &in, int &line) {}*/
+
+/*bool Expr(istream &in, int &line) {}*/
+
+/*bool LogANDExpr(istream &in, int &line) {}*/
+
+/*bool EqualExpr(istream &in, int &line) {}*/
+
+/*bool RelExpr(istream &in, int &line) {}*/
+
+/*bool AddExpr(istream &in, int &line) {}*/
+
+/*bool MultExpr(istream &in, int &line) {}*/
+
+/*bool UnaryExpr(istream &in, int &line) {}*/
+
+/*bool PrimaryExpr(istream &in, int &line, int sign) {}*/
